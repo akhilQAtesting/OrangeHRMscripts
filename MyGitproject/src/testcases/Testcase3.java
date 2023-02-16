f40296696293a9e60e514cf0d7b04a055090d937
Akhil Godavarthi <akhilgodavarthi88@gmail.com>
2023-02-16 12:11:50 +0530

    third commit:test case 3 added

---
+++ MyGitproject/src/testcases/Testcase3.java
@@ -0,0 +1,13 @@
+package testcases;
+
+import java.util.concurrent.SynchronousQueue;
+
+public class Testcase3 {
+
+	public static void main(String[] args) {
+		System.out.println("Testcase 3");
+	
+
+	}
+
+}
